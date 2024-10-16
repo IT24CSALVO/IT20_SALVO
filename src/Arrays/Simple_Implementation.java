8b73b33f32df6f748d52dda00e1805290f6fbb87
IT24CSALVO <alliahshanesalvo11@gmail.com>
2024-10-16 12:26:53 +0800

    simple_implementation

--- src/Arrays/Simple_Implementation.java
+++ src/Arrays/Simple_Implementation.java
@@ -23,5 +23,33 @@ public class Simple_Implementation {
          Black
          Red */
        
+        //declaring numbers inside array"
+       int [] numValue = {50,45,30,60,85,100};
+       
+       //procedure
+       System.out.println("The number inside the array is : " + numValue.length);
+       
+       //output
+       //The number inside the array is : 6
+       
+       
+       //declaring numbers inside array"
+       int[] num = {4,5,8,16,20};
+       
+       //procedure
+        System.out.println("Sum : " + (num[1] + num[2]));
+        System.out.println("Difference : " + (num[3] - num[1]));
+        System.out.println("Product : " + (num[3] * num[2]));
+        System.out.println("Quotient : " + (num[4] / num[3]));
+        
+        //output
+       /* 
+        Sum : 18
+        Difference : 11
+        Product : 128
+        Quotient : 1
+        */
+      
     }
+    
 }
